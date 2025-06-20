a3ab5a3811eefd60c1d79bb339b6a5ef64a6273d
RichardNguyen <21522902@gm.uit.edu.vn>
2025-06-20 21:23:18 +0700

    abc

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Baitap1 from "./baitap1"
+import Baitap2 from "./baitap2";
+
+
+function App() {
+  return (
+    <>
+      <h1>Learn Reactjs</h1>
+      <Baitap1/>
+      <Baitap2/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/baitap2/index.jsx
@@ -0,0 +1,17 @@
+import Carousel from "./carousel";
+import Footer from "./footer";
+import Header from "./header";
+import ListItem from "./list-item";
+
+const Baitap2 = () => {
+  return (
+    <div>
+      <Header />
+      <Carousel />
+      <ListItem />
+      <Footer />
+    </div>
+  );
+};
+
+export default Baitap2;
